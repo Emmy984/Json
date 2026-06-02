b4d3dd5ae870261854404db061945e9023764cc5
Emmy984 <fsremily01@gmail.com>
2026-06-02 13:34:51 -0300

    aula-02

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const usuarios = JSON.parse(dados);
 
 console.log("usuarios cadastrados");
 console.log(usuarios)
+
+usuarios.push({id:3,nome:"Carlos",idade:14})
+
+fs.writeFileSync('dados.json',JSON.stringify(usuarios, null,2))
